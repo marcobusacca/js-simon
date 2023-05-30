6a11f1efc05a83c40f2828c7ca703793adb46454
marcobusacca <marcobusacca01@gmail.com>
2023-05-30 17:23:32 +0200

    [FEAT] Js Function createRandomNumber

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+// FUNZIONE CHE GENERA UN NUMERO CASUALE
+function createRandomNumber(min, max){
+
+    // CREA UN NUMERO RANDOM E LO RESTITUISCE
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
