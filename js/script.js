7398612ada9f0f3b81dfdeaef4cfeb5f4c2f0e9f
marcobusacca <marcobusacca01@gmail.com>
2023-05-30 17:25:36 +0200

    [FEAT] Js Function checkRandomNumber

--- js/script.js
+++ js/script.js
@@ -6,3 +6,26 @@ function createRandomNumber(min, max){
     // CREA UN NUMERO RANDOM E LO RESTITUISCE
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+// FUNZIONE CHE CONTROLLA OGNI NUMERO RANDOM GENERATO
+function checkRandomNumber(arrayNumber){
+
+    // VARIABILE DI CONTROLLO
+    let checkNumber = false;
+
+    // VARIABILE CHE CONTIENE IL NUMERO CASUALE
+    let randomNumber;
+
+    while (checkNumber === false){
+
+        // GENERO IL NUMERO CASUALE
+        randomNumber = createRandomNumber(1, 100);
+
+        if(!arrayNumber.includes(randomNumber)){ // L'ARRAY_NUMBER NON INCLUDE IL NUMERO CASUALE GENERATO
+
+            numberCheck = true;
+        }
+    }
+
+    return randomNumber;
+}
